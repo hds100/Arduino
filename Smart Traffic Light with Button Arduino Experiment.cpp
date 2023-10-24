741674b1f390849f4465623a5ad930e10ead8a4e
João Pedro H <148555673+hds100@users.noreply.github.com>
2023-10-24 19:31:51 -0300

    Smart Traffic Light with Button Arduino Experiment.cpp

---
+++ Smart Traffic Light with Button Arduino Experiment.cpp
@@ -0,0 +1,59 @@
+// Code in C++
+//
+
+/*
+  Configuration of LED and Button output ports
+
+  11 = Green
+  12 = Yellow
+  13 = Red
+  7 = Red (pedestrian)
+  6 = Green (pedestrian)
+  2 = Button (button for stop traffic lighting)
+
+
+*/
+void setup()
+{
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(2, INPUT);
+}
+/*
+11 - Green = ON
+7 - Red (pedestrian) = ON
+
+If = When the pedestrian presses the button, the traffic light for vehicles from green (11) changes to yellow (gate 12), and then to red (13). For pedestrians, it changes to green (gate 6), a descent that can cross the lane.
+
+Wait 30 seconds
+
+Loop back to initial situation = green for vehicles and red for pedestrians
+
+  */
+void loop()
+{
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(6, LOW);
+  if (digitalRead(2) == 0) {
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    delay(3000); // Wait for 3000 millisecond(s)
+    digitalWrite(12, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(6, HIGH);
+    digitalWrite(13, HIGH);
+    delay(6000); // Wait for 6000 millisecond(s)
+    digitalWrite(13, HIGH);
+    delay(60000); // Wait for 30000 millisecond(s)
+    digitalWrite(13, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+  }
+}
